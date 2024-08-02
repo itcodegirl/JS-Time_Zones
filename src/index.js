a7d845e7ebb3b8fbd6747c272802733dcd7be53d
Jenna <88835851+itcodegirl@users.noreply.github.com>
2024-08-02 12:53:50 -0500

    Update index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+
+
+//Asia, Tokyo time via moment-timezone
+
 let tokyoTime = moment()
 	.tz("Asia/Tokyo")
 	.format("dddd, MMMM D, YYYY h:m A");
@@ -6,8 +10,20 @@ let tokyoElement = document.querySelector("#tokyo");
 
 tokyoElement.innerHTML = tokyoTime;
 
+//America, New York time via moment-timezone
+
 let localTimezone = moment.tz.guess();
 let localElement = document.querySelector("#local");
 localElement.innerHTML = `Your current time zone is ${localTimezone} and the current time is ${moment().format(
 	"h:m A"
 )}`;
+
+//Europe, Athens time via moment-timezone
+
+let athensTime = moment().tz("Europe/Athens").format("dddd, MMMM D, YYYY h:m A");
+
+let athensElement = document.querySelector("#athens");
+
+athensElement.innerHTML = `The current date and time in Athens Greece is ${athensTime}`;
+
+
